ee480728493cc6f2f0a02d977482d8202ec2ce93
chivo <propugnatorrr@gmail.com>
2026-08-28 14:32:43 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type {
+  NextConfig,
+} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          '**.public.blob.vercel-storage.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
